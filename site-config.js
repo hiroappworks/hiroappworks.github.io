db1fca6fa157885a1997f3c828a7bf918d683ae2
Hiro App Works <hiro.appworks@gmail.com>
2026-08-18 14:49:00 +0900

    Finalize Hiro App Works site for launch

--- site-config.js
+++ site-config.js
@@ -5,7 +5,7 @@
 window.HIRO_APP_WORKS_CONFIG = Object.freeze({
   apps: Object.freeze({
     consignmentNote: Object.freeze({
-      appStoreUrl: "",
+      appStoreUrl: "https://apps.apple.com/jp/app/%E5%A7%94%E8%A8%97%E8%B2%A9%E5%A3%B2%E3%83%8E%E3%83%BC%E3%83%88/id6800209010",
     }),
   }),
 });
