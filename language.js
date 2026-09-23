158046b1a992c970bfdba2d8c0e052212e131a8f
Hiro App Works <hiro.appworks@gmail.com>
2026-09-23 09:06:27 +0900

    Add stable privacy policy URLs

--- language.js
+++ language.js
@@ -9,11 +9,13 @@
     "/": {ja: "/", en: "/en/"},
     "/privacy": {ja: "/privacy/", en: "/en/privacy/"},
     "/consignment-note-privacy": {ja: "/consignment-note-privacy/", en: "/en/consignment-note-privacy/"},
+    "/consignment-note/privacy": {ja: "/consignment-note/privacy/", en: "/en/consignment-note/privacy/"},
     "/contact": {ja: "/contact/", en: "/en/contact/"},
     "/contact/thanks": {ja: "/contact/thanks/", en: "/en/contact/thanks/"},
     "/en": {ja: "/", en: "/en/"},
     "/en/privacy": {ja: "/privacy/", en: "/en/privacy/"},
     "/en/consignment-note-privacy": {ja: "/consignment-note-privacy/", en: "/en/consignment-note-privacy/"},
+    "/en/consignment-note/privacy": {ja: "/consignment-note/privacy/", en: "/en/consignment-note/privacy/"},
     "/en/contact": {ja: "/contact/", en: "/en/contact/"},
     "/en/contact/thanks": {ja: "/contact/thanks/", en: "/en/contact/thanks/"}
   };
